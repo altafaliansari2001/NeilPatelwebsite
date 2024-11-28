6b03fb0b00e670a32427933454d12dd61b5e7a0b
altaf ali <altafaliansari092001@gmail.com>
2024-11-28 22:05:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector(".p-submit");
+
+btn.addEventListener("mouseover", function(){
+        btn.textContent = "Processing..."
+        btn.style.color = "white"
+        btn.style.backgroundColor = "red"
+        btn.style.padding = "15px"
+        btn.style.width= "295px"
+   
+})
